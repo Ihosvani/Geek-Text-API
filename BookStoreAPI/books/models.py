90f56671127ad0edee4ec297949bff5fbdac128b
Britnie Ojie-Ahamiojie <bojieahamiojie@gmail.com>
2022-07-04 16:39:05 -0500

    Added profile to source code

--- BookStoreAPI/books/models.py
+++ BookStoreAPI/books/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.forms import EmailField
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+
 class Books(models.Model):
     ISBN = models.CharField(primary_key=True, max_length=13)
     bookTitle = models.CharField(max_length=500)
@@ -43,3 +46,14 @@ class Authors(models.Model):
     def __str__(self):
         return self.authorFirstName + ' ' + self.authorLastName
     
+class Profile(models.Model):
+    username = models.EmailField(max_length=100, unique=True, blank=False)
+    password = models.CharField(max_length=20, blank=False)
+    firstName = models.CharField(max_length=100, blank=False)
+    lastName = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(max_length=100, blank=False)
+    homeAddress = models.CharField(max_length=500, blank=False)
+    creditCard = models.PositiveIntegerField(validators=[MaxValueValidator(9999999999999999)], unique=True, blank=False)
+
+def __str__(self):
+    return self.username
