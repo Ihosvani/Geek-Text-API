277156034483ab50bdb730452d761607ccf48a2e
Ihosvani <ihosvani2014@gmail.com>
2022-07-11 10:34:39 -0400

    Changed the model Ratings and comments to have a relationship with Profile model

--- BookStoreAPI/books/models.py
+++ BookStoreAPI/books/models.py
@@ -1,8 +1,18 @@
-import re
-import string
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.EmailField(max_length=100, unique=True, blank=False)
+    password = models.CharField(max_length=20, blank=False)
+    firstName = models.CharField(max_length=100, blank=False)
+    lastName = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(max_length=100, blank=False)
+    homeAddress = models.CharField(max_length=500, blank=False)
+
+    def __str__(self):
+        return self.username
 
 class Books(models.Model):
     ISBN = models.CharField(primary_key=True, max_length=13)
@@ -22,19 +32,19 @@ class Comments(models.Model):
     ISBN_COMMENT= models.ManyToManyField(Books)
     bookComment = models.CharField(max_length=10000)
     commentDate = models.DateField(auto_now_add=True, blank=True)
-    userName = models.CharField(max_length=50)
+    userId = models.ManyToManyField(Profile)
 
     def __str__(self):
-        return self.ISBN
+        return self.ISBN_COMMENT
 
 class Ratings(models.Model):
     ISBN_RATING = models.ManyToManyField(Books)
-    rating = models.CharField(max_length=1)
+    rating = models.IntegerField(max_length=1)
     ratingDate = models.DateField(auto_now_add=True, blank=True)
-    userName = models.CharField(max_length=50)
+    userId = models.ManyToManyField(Profile)
 
     def __str__(self):
-        return self.ISBN
+        return self.ISBN_RATING
 
 class Authors(models.Model):
     id = models.AutoField(primary_key=True)
@@ -45,23 +55,11 @@ class Authors(models.Model):
     
     def __str__(self):
         return self.authorFirstName + ' ' + self.authorLastName
-    
-class Profile(models.Model):
-    id = models.AutoField(primary_key=True)
-    username = models.EmailField(max_length=100, unique=True, blank=False)
-    password = models.CharField(max_length=20, blank=False)
-    firstName = models.CharField(max_length=100, blank=False)
-    lastName = models.CharField(max_length=100, blank=False)
-    email = models.EmailField(max_length=100, blank=False)
-    homeAddress = models.CharField(max_length=500, blank=False)
-
-def __str__(self):
-    return self.username
 
 class Payment(models.Model):
     username_creditCard = models.ManyToManyField(Profile)
     bankName = models.CharField(max_length=100, blank=False)
     creditCard = models.CharField(max_length=16, unique=True, blank=False)
 
-def __str__(self):
-    return self.bankName + ' ending in ' + self.creditCard[-4:]
+    def __str__(self):
+        return self.bankName + ' ending in ' + self.creditCard[-4:]
